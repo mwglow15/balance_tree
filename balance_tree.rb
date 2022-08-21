e0dac98b2967d0fdfc1a4159421094ef4657117d
mattwg <mattwglowacki@gmail.com>
2022-08-21 19:50:34 -0400

    Create Tree Class

--- balance_tree.rb
+++ balance_tree.rb
@@ -1,3 +1,15 @@
+def class Tree
+
+  def initialize(array)
+    @root = build_tree(array)
+  end
+
+  def build_tree(array)
+    
+  end
+
+end
+
 def class Node
   attr_accessor :data, :left_node, :right_node
 
