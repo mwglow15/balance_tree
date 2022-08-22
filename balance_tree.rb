6d541a4e55475dcf86c25508ace953ac0edbae06
mattwg <mattwglowacki@gmail.com>
2022-08-21 22:20:55 -0400

    Begin build_tree method

--- balance_tree.rb
+++ balance_tree.rb
@@ -1,16 +1,28 @@
-def class Tree
+class Tree
 
   def initialize(array)
-    @root = build_tree(array)
+    p array.sort
+    @root = build_tree(array.sort)
   end
 
   def build_tree(array)
+    array_start = 0
+    array_end = array.length
+    mid = array.length / 2
+
+    return nil if array_start >= mid
+
+    node = Node.new(array[mid-1])
     
+    node.left_node = build_tree(array[array_start,mid])
+    node.right_node = build_tree(array[mid, array_end])
+
+    node
   end
 
 end
 
-def class Node
+class Node
   attr_accessor :data, :left_node, :right_node
 
   def initialize(data)
@@ -19,3 +31,6 @@ def class Node
     @right_node = nil
   end
 end
+
+a = Tree.new([1, 6, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324])
+p a
